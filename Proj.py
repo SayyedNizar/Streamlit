3a417d9ebb5283c60f7db79ae7251991ad01adaa
SayyedNizar <151517789+SayyedNizar@users.noreply.github.com>
2024-10-16 15:01:35 +0530

    main
    updated in Proj.py

--- Proj.py
+++ Proj.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My First Streamlit App")
-st.title("I am Nizar")
-st.title("hello world")
+st.header("This is Nizar")
+st.write("hello world")
+data = {
+    'Name':['NIzar','sabari','nishanth'],
+    'Age':[20,28,48]
+
+}
+df=pd.DataFrame(data)
+st.dataframe(df)
+name=st.text_input("Enter your name")
+st.write(f"Hello!,{name}!")
